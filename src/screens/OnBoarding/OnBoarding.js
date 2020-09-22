846f6eefef3848ddb9ca6ebc4be2c7667896f2f4
Benjamin Daniel <benjamindaniel706@gmail.com>
2020-09-22 21:00:15 +0100

    added basic slider

--- src/screens/OnBoarding/OnBoarding.js
+++ src/screens/OnBoarding/OnBoarding.js
@@ -1,11 +1,26 @@
 import React from 'react';
 
+import Swiper from 'react-native-web-swiper';
+
 import {Box, Text} from 'components';
+const OnBoardContainer = ({children}: {children?: React.FC}) => (
+  <Box flex={1} alignItems="center" justifyContent="center">
+    {children}
+  </Box>
+);
 
 export const OnBoarding = () => {
   return (
-    <Box>
+    <Box flex={1}>
       <Text>Hey</Text>
+      <Swiper>
+        <OnBoardContainer>
+          <Text>hey</Text>
+        </OnBoardContainer>
+        <OnBoardContainer>
+          <Text>hey</Text>
+        </OnBoardContainer>
+      </Swiper>
     </Box>
   );
 };
