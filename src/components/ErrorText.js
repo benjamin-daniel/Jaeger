3289d93a8f6d125b362dc08be2967214de665bf3
Benjamin Daniel <benjamindaniel706@gmail.com>
2020-09-24 05:33:18 +0100

    refactored errortext code

--- src/components/ErrorText.js
+++ src/components/ErrorText.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import {Text} from './Text';
 
-export const ErrorText = ({error, touched, ErrorTextStyles}) => (
-  <>
-    {touched && error && (
-      <Text fontSize={12} color="error" style={[ErrorTextStyles]}>
-        {error}
-      </Text>
-    )}
-  </>
-);
+export const ErrorText = ({error, touched, ErrorTextStyles}) => {
+  const condition = touched && error;
+  if (!condition) {
+    return null;
+  } else {
+    return (
+      <>
+        <Text fontSize={12} color="error" style={ErrorTextStyles}>
+          {error}
+        </Text>
+      </>
+    );
+  }
+};
